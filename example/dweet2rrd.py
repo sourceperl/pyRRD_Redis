c768220f653e0e0c867aee9fecb144e5973f1c5e
sourceperl <loic.celine@free.fr>
2017-04-25 18:01:55 +0200

    add dweet src example

---
+++ example/dweet2rrd.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+from pyRRD_Redis import RRD_redis, StepAddFunc
+import json
+import socket
+import urllib.parse
+import urllib.request
+import urllib.error
+
+
+# some class
+class Dweet(object):
+    HEADS = {'Content-type': 'application/json'}
+    UPDATE_URL = 'https://dweet.io/dweet/for/'
+    GET_URL = 'https://dweet.io/get/latest/dweet/for/'
+
+    def __init__(self, dweet_id):
+        self.dweet_id = str(dweet_id)
+        # set timeout to 10 seconds (default is none)
+        socket.setdefaulttimeout(10)
+
+    def update(self, fields):
+        # do update request
+        try:
+            request = urllib.request.Request(Dweet.UPDATE_URL + self.dweet_id, headers=Dweet.HEADS,
+                                             data=json.dumps(fields).encode('utf-8'))
+            urllib.request.urlopen(request)
+            return True
+        except urllib.error:
+            return False
+
+    def get(self):
+        # do get request
+        try:
+            request = urllib.request.Request(Dweet.GET_URL + self.dweet_id, headers=Dweet.HEADS)
+            response = urllib.request.urlopen(request)
+            data = response.read().decode('utf-8')
+            return json.loads(data)
+        except urllib.error:
+            return None
+
+# init
+rrd = RRD_redis('', size=8640, step=10.0, add_func=StepAddFunc.avg)
+dw = Dweet(dweet_id='0d328b86-fcba-469e-a31a-adfad51be68a')
+
+# main loop
+while True:
+    try:
+        # get dweet json msg as dict
+        d = dw.get()['with'][0]['content']
+        # setRRD for all dweet floats vars
+        for k in d:
+            rrd.name = 'rrd:' + k
+            try:
+                rrd.add_step(float(d[k]))
+            except ValueError:
+                pass
+    except (IndexError, TypeError):
+        pass
+    # wait for next loop
+    time.sleep(10.0)
